811ec1a0b26627b40788e60a41fd67a3047dcc34
Robin Moreno Rinding <robin.moreno.rinding@gmail.com>
2018-11-27 17:04:07 +0100

    Update adventofcode_day_1_part_1.cpp

--- adventofcode_day_1_part_1.cpp
+++ adventofcode_day_1_part_1.cpp
@@ -9,15 +9,15 @@ int main()
     string x;
     int sum = 0;
     cin >> x;
-    int size = (int)x.size();
+    int size = (int)x.size(); //length of input
     for(int i = 0; i < size; i++){
         if(i == 0){
             if(x[i] == x[size - 1]){
-                sum += ((int)x[i] - 48);
+                sum += ((int)x[i] - 48); //Convert from char to int
             } 
         }
         else if(x[i] == x[i - 1]){
-            sum += ((int)x[i] - 48);
+            sum += ((int)x[i] - 48); //Convert from char to int
         }
         
     }
