b4574c8a1a960544084bd620f82faa32e65376ac
RobinMoRi <robin.moreno.rinding@gmail.com>
2022-12-07 14:23:12 +0100

    cleaned solution a bit

--- 2022/7/part1.js
+++ 2022/7/part1.js
@@ -1,37 +1,11 @@
 const path = require('path');
 const util = require('util');
 const { syncReadFile } = require('../readFile')
+const { parseFileSys } = require('./utils')
 
 const mode = process.argv[2];
 const inputFile = mode === 'test' ? 'test.txt' : 'input.txt';
 const input = syncReadFile(path.join(__dirname, inputFile))
-
-// construct data structure to traverse
-function parseFileSys(){
-    const fileSys = {};
-    let dirStack = [];
-    for(cmd of input){
-        const currentDir = dirStack[dirStack.length-1] ? String(dirStack[dirStack.length-1]) : '';
-        const currentPath = dirStack.join('/');
-        if(currentDir !== '' && !fileSys[currentPath]) fileSys[currentPath] = {totalFileSize: 0, dirs: []};
-    
-        //If we go into a dir:
-        const cdInMatch = cmd.match(/cd ([a-z\/]+)/);
-        const cdOutMatch = cmd.match(/cd (\.\.)/);
-        const fileMatch = cmd.match(/([0-9]+) ([a-z.]+)/);
-        const dirMatch = cmd.match(/dir ([a-z]+)/);
-        if(cdInMatch !== null) dirStack.push(cdInMatch[1]);
-        else if(cdOutMatch) dirStack.pop();
-        else if(fileMatch){
-            const currentFileSize = parseInt(fileMatch[1]);
-            if(currentDir !== '') fileSys[currentPath].totalFileSize += currentFileSize;
-        }
-        else if(dirMatch){
-            if(currentDir !== '') fileSys[currentPath].dirs.push(dirStack.join('/') + "/" + dirMatch[1]);
-        }
-    }
-    return fileSys;
-}
 
 //Traverse tree to add file size of a dir's subdirs
 function treeTraversal(node) {
@@ -46,7 +20,7 @@ function treeTraversal(node) {
 
 function sumFileSizes(){
     let sum = 0;
-    for(let [dir, content] of Object.entries(fileSys)){
+    for(let [ dir ] of Object.entries(fileSys)){
         if(fileSys[dir].totalFileSize <= 100000){
             sum += fileSys[dir].totalFileSize;
         };
@@ -54,6 +28,6 @@ function sumFileSizes(){
     return sum;
 }
 
-let fileSys = parseFileSys();
+let fileSys = parseFileSys(input);
 treeTraversal(fileSys['/']);
 console.log(sumFileSizes())
--- 2022/7/utils.js
+++ 2022/7/utils.js
@@ -1,7 +1,37 @@
-function template() {
-    return null;
+// Regexes
+const CD_DOWN_REGEX = /cd ([a-z\/]+)/;
+const CD_UP_REGEX = /cd (\.\.)/;
+const FILE_REGEX = /([0-9]+) ([a-z.]+)/;
+const DIR_REGEX = /dir ([a-z]+)/;
+
+// construct data structure to traverse
+function parseFileSys(input){
+    const fileSys = {};
+    let dirStack = [];
+    for(cmd of input){
+        // Get current directory and full path
+        const currentDir = dirStack[dirStack.length-1] ? String(dirStack[dirStack.length-1]) : '';
+        const currentPath = dirStack.join('/');
+        if(currentDir !== '' && !fileSys[currentPath]) fileSys[currentPath] = {totalFileSize: 0, dirs: []};
+    
+        //Parse row
+        const cdInMatch = cmd.match(CD_DOWN_REGEX);
+        const cdOutMatch = cmd.match(CD_UP_REGEX);
+        const fileMatch = cmd.match(FILE_REGEX);
+        const dirMatch = cmd.match(DIR_REGEX);
+        if(cdInMatch !== null) dirStack.push(cdInMatch[1]);
+        else if(cdOutMatch) dirStack.pop();
+        else if(fileMatch && currentDir !== ''){
+            const currentFileSize = parseInt(fileMatch[1]);
+            fileSys[currentPath].totalFileSize += currentFileSize;
+        }
+        else if(dirMatch && currentDir !== ''){
+            fileSys[currentPath].dirs.push(dirStack.join('/') + '/' + dirMatch[1]);
+        }
+    }
+    return fileSys;
 }
 
 module.exports = {
-    template,
+    parseFileSys,
 };
