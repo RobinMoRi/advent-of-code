f186113c87df5509fa36105aecac1ccdbce4bc1f
Robin <robin.moreno.rinding@betao.se>
2024-12-02 16:27:32 +0100

    aoc: add solution for part 2

--- 2024/02/solution.py
+++ 2024/02/solution.py
@@ -1,40 +1,82 @@
-def get_lines(path: str):
+import os
+
+
+def get_lines(filename: str):
+    this_file = os.path.abspath(__file__)
+    this_dir = os.path.dirname(this_file)
+    filepath = os.path.join(this_dir, filename)
     lines = []
-    with open(path, "r") as file:
+    with open(filepath, "r") as file:
         for line in file:
             lines.append(line.strip().split())
     return lines
 
 
-def part_1(path: str = "02/input-1.txt"):
-    lines = get_lines(path)
+def is_safe(nums: list[int]):
 
-    # Naive approach
-    total_unsafe = 0
+    should_decrease = (nums[-1] - nums[0]) < 0
+    diffs = []
+    for i in range(1, len(nums)):
+        first = nums[i - 1]
+        second = nums[i]
+
+        if should_decrease and (second - first) > 0:
+            return False, i
+
+        if not should_decrease and (second - first) < 0:
+            return False, i
+
+        if abs(second - first) < 1 or abs(second - first) > 3:
+            return False, i
+
+        diffs.append(second - first)
+        should_decrease = (second - first) < 0
+
+    return True, i
+
+
+def part_1(file: str = "input-1.txt"):
+    lines = get_lines(file)
+
+    # Naive approach ??
+    total_safe = 0
     for line in lines:
-        should_decrease = (int(line[1]) - int(line[0])) < 0
-        for i in range(1, len(line)):
-            first = int(line[i - 1])
-            second = int(line[i])
+        res = is_safe([int(num) for num in line])
+        is_line_safe = res[0]
+        if is_line_safe:
+            total_safe += 1
 
-            if should_decrease and (second - first) > 0:
-                total_unsafe += 1
-                print(second, first, abs(second - first))
-                break
-
-            if abs(second - first) < 1 or abs(second - first) > 3:
-                total_unsafe += 1
-                print(second, first, abs(second - first))
-                break
-
-    total_safe = len(lines) - total_unsafe
+    print("---- Part 1 ----")
     print(f"Total safe: {total_safe}")
-    print(f"Total unsafe: {total_unsafe}")
     return total_safe
 
 
-def part_2():
-    pass
+def part_2(file: str = "input-1.txt"):
+    lines = get_lines(file)
+
+    def _is_safe_inner(nums: list[int]):
+        res = is_safe(nums)
+        is_line_safe = res[0]
+        idx = res[1]
+        if not is_line_safe:
+            if nums[-1] == nums[idx]:
+                return False
+            new_nums = nums[0:idx] + nums[idx + 1 :]
+            print(new_nums)
+            return _is_safe_inner(new_nums)
+        return True
+
+    # Naive approach ??
+    total_safe = 0
+    for line in lines:
+        nums = [int(num) for num in line]
+
+        if _is_safe_inner(nums):
+            total_safe += 1
+
+    print("---- Part 2 ----")
+    print(f"Total safe: {total_safe}")
+    return total_safe
 
 
 if __name__ == "__main__":
--- 2024/02/test_day_2.py
+++ 2024/02/test_day_2.py
@@ -1,5 +1,9 @@
-from solution import part_1
+from solution import part_1, part_2
 
 
 def test_part_1():
-    assert part_1("02/test.txt") == 2
+    assert part_1("test.txt") == 2
+
+
+def test_part_2():
+    assert part_2("test.txt") == 4
